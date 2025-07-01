1fc7870230b3d7782f09ea5d1b45732039f0c921
Girish Chaudhari <girish.chaudhari@visaero.com>
2025-07-01 10:13:04 +0530

    Implement new presentation card with form validation and dialog; add input, label, and form components

--- apps/web/components/new-presentation-card.tsx
+++ apps/web/components/new-presentation-card.tsx
@@ -1,14 +1,100 @@
 "use client";
 
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { PlusIcon } from "lucide-react";
 import { AnimatedCard } from "./animated-card";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@workspace/ui/components/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@workspace/ui/components/form";
+
+import { Input } from "@workspace/ui/components/input";
+import { Button } from "@workspace/ui/components/button";
+import { useState } from "react";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Presentation name must be at least 2 characters.",
+  }),
+});
+
 export const NewPresentationCard = () => {
+  const [openNewPPT, setOpenNewPPT] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    setOpenNewPPT(false)
+  }
+
   return (
-    <AnimatedCard>
-      <div className="text-muted-foreground group-hover:text-primary flex h-full w-full items-center justify-center transition-colors duration-300">
-        <PlusIcon size={32} strokeWidth={2.5} />
-      </div>
-    </AnimatedCard>
+    <>
+      <Dialog open={openNewPPT} onOpenChange={setOpenNewPPT}>
+        <AnimatedCard>
+          <div
+            onClick={() => setOpenNewPPT(true)}
+            className="text-muted-foreground group-hover:text-primary flex h-full w-full items-center justify-center transition-colors duration-300"
+          >
+            <PlusIcon size={32} strokeWidth={2.5} />
+          </div>
+        </AnimatedCard>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Presentation</DialogTitle>
+            <DialogDescription>
+              Start building a new PowerPoint presentation. You can choose a template or begin from
+              scratch.
+            </DialogDescription>
+          </DialogHeader>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Presentation Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Presentation Name" {...field} />
+                    </FormControl>
+                    <FormDescription> This will be the title of your presentation.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogFooter>
+                <Button type="submit">Submit</Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
--- apps/web/components/presentation-preview-card.tsx
+++ apps/web/components/presentation-preview-card.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { Button } from "@workspace/ui/components/button";
+import { TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { AnimatedCard } from "./animated-card";
 
 export const PresentationPreviewCard = ({ data }: { data: number }) => {
   return (
     <AnimatedCard>
-      <div className="relative w-full h-full">
+      <div className="relative h-full w-full">
+        <Button size="icon" variant="outline" className="absolute right-1 top-1 z-10">
+          <TrashIcon />
+        </Button>
+
         <Image
           src={`https://picsum.photos/200/300?random=${data}`}
           alt={`preview-url-${data}`}
